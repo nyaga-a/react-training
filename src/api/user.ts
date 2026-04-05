05f595b376f943760441f21e6b6773c38902f693
nyaga-a <naga.asuka@gamil.com>
2026-04-05 21:44:29 +0900

    Add User interface for API response

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,13 +1,8 @@
 import axious from 'axios'
-
-export type User = {
-    id: number
-    name: string
-    email: string
-}
+import { type User } from '../types/User'
 
 export const fetchUsers = async () : Promise<User[]> => {
-    const res = await axious.get(
+    const res = await axious.get<User[]>(
         'https://jsonplaceholder.typicode.com/users'
     )
     return res.data
--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchUsers } from "../api/user";
+import { type User } from "../types/User"
 
 export const useUsers = () => {
-    return useQuery({
+    return useQuery<User[]>({
         queryKey: ['users'],
         queryFn: fetchUsers,
         staleTime: 10000,
